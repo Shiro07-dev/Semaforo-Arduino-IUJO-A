5565e90441ec62e4efe3573abffe481e3ff80c71
Shiro07-dev <shirokamikaze03@gmail.com>
2026-08-02 10:26:54 -0400

    Definicion de pines y configuracion

---
+++ semaforo.ino
@@ -0,0 +1,27 @@
+int Semaforo_Rojo_1 = 12;
+int Semaforo_Amarillo_1 = 11;
+int Semaforo_Verde_1 = 10;
+int Peatonal_Rojo_1 = 9;
+int Peatonal_Verde_1 = 8;
+int Semaforo_Rojo_2 = 7;
+int Semaforo_Amarillo_2 = 6;
+int Semaforo_Verde_2 = 5;
+int Peatonal_Rojo_2 = 4;
+int Peatonal_Verde_2 = 3;
+
+void setup() {
+  pinMode(Semaforo_Rojo_1,      OUTPUT);
+  pinMode(Semaforo_Amarillo_1,  OUTPUT);
+  pinMode(Semaforo_Verde_1,     OUTPUT);
+  pinMode(Peatonal_Rojo_1,      OUTPUT);
+  pinMode(Peatonal_Verde_1,     OUTPUT);
+  pinMode(Semaforo_Rojo_2,      OUTPUT);
+  pinMode(Semaforo_Amarillo_2,  OUTPUT);
+  pinMode(Semaforo_Verde_2,     OUTPUT);
+  pinMode(Peatonal_Rojo_2,      OUTPUT);
+  pinMode(Peatonal_Verde_2,     OUTPUT);
+}
+
+void loop() {
+  // Próxima fase: lógica vehicular
+}
