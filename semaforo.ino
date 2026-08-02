0113b302aacc99e3c1e21649fbbd65b8ebe0aa4f
Susej25 <susejvq25@gmail.com>
2026-08-02 10:43:35 -0400

    Codigo final completo y peatones Este-Oeste

--- semaforo.ino
+++ semaforo.ino
@@ -1,11 +1,14 @@
 int Semaforo_Rojo_1 = 12;
 int Semaforo_Amarillo_1 = 11;
 int Semaforo_Verde_1 = 10;
+
 int Peatonal_Rojo_1 = 9;
 int Peatonal_Verde_1 = 8;
+
 int Semaforo_Rojo_2 = 7;
 int Semaforo_Amarillo_2 = 6;
 int Semaforo_Verde_2 = 5;
+
 int Peatonal_Rojo_2 = 4;
 int Peatonal_Verde_2 = 3;
 
@@ -23,17 +26,15 @@ void setup() {
 }
 
 void loop() {
-  // FASE NORTE-SUR: VERDE
   digitalWrite(Semaforo_Verde_1,     HIGH);
   digitalWrite(Peatonal_Rojo_1,      HIGH);
   digitalWrite(Semaforo_Rojo_2,      HIGH);
   digitalWrite(Peatonal_Verde_2,     HIGH);
-  delay(3000);
+  delay(3000);                              
 
-  // FASE NORTE-SUR: AMARILLO
   digitalWrite(Semaforo_Verde_1,      LOW);
   digitalWrite(Semaforo_Amarillo_1,  HIGH);
-  delay(1000);
+  delay(1000);                              
 
   digitalWrite(Semaforo_Amarillo_1,   LOW);
   digitalWrite(Peatonal_Rojo_1,       LOW);
@@ -41,10 +42,17 @@ void loop() {
   digitalWrite(Peatonal_Verde_2,      LOW);
 
   digitalWrite(Semaforo_Rojo_1,      HIGH);
+  digitalWrite(Peatonal_Verde_1,     HIGH);
   digitalWrite(Semaforo_Verde_2,     HIGH);
-  delay(3000);
+  digitalWrite(Peatonal_Rojo_2,      HIGH);
+  delay(3000);                             
 
   digitalWrite(Semaforo_Verde_2,      LOW);
   digitalWrite(Semaforo_Amarillo_2,  HIGH);
-  delay(1000);
+  delay(1000);                             
+
+  digitalWrite(Semaforo_Rojo_1,       LOW);
+  digitalWrite(Peatonal_Verde_1,      LOW);
+  digitalWrite(Semaforo_Amarillo_2,   LOW);
+  digitalWrite(Peatonal_Rojo_2,       LOW);
 }
